b27212fdcee5a01c2872b99bd84a5236ae48f4b5
Anse98 <lorenzo.anselmi98.com>
2023-10-19 16:47:00 +0200

    Ottimizzato codice

--- app.js
+++ app.js
@@ -1,4 +1,73 @@
 
+//////////////////////////// FUNZIONI ///////////////////////////////////////
+
+
+//FUNZIONE PER SCORRERE LE FOTO A DESTRA DEL CAROSELLO
+function swipeRight () {
+
+  imgContainerDOMEl[currentImgIndex].classList.remove("active");
+
+  if(currentImgIndex === images.length - 1) {
+
+    currentImgIndex = 0;
+
+  } else {
+
+    currentImgIndex ++;
+
+  }
+   
+  imgContainerDOMEl[currentImgIndex].classList.add("active");
+  
+}
+
+
+// FUNZIONE PER SCORRERE LE FOTO A SINISTRA DEL CAROSELLO
+function swipeLeft() {
+
+  imgContainerDOMEl[currentImgIndex].classList.remove("active");
+
+  if(currentImgIndex === 0) {
+
+    currentImgIndex = images.length -1;
+
+  } else {
+
+    currentImgIndex --;
+
+  }
+
+  imgContainerDOMEl[currentImgIndex].classList.add("active");
+
+}
+
+
+// FUNZIONE CHE MI STAMPA L'ARRAY IN HTML NELL'ELEMENTO CONTAINER
+function printArrayInHtml(array) {
+
+for(let i = 0; i < array.length; i++) {
+  const image = array[i];
+  
+  const containerDOMEl = document.querySelector(".container");
+
+  containerDOMEl.innerHTML += 
+  ` 
+  <div class="img-container">
+    <img src="${image.image}">
+    <div class="img-text">
+    <h2>${image.title}</h2>
+    <p>${image.text}</p>
+    </div>
+  </div>
+  ` ;
+} 
+}
+
+
+//////////////////////////////// FINE FUNZIONI /////////////////////////////////
+
+
+
 // Array di oggetti 
 const images = [
 	{
@@ -32,38 +101,22 @@ const images = [
 	},
 ]
 
+// Stampo nell'html l'array di oggetti
+printArrayInHtml(images);
 
 
-// vado a prendermi ogni oggetto uno alla volta
-for(let i = 0; i < images.length; i++) {
-  const image = images[i];
-  
-  const containerDOMEl = document.querySelector(".container");
-
-  // ogni oggetto lo stampo nell'html
-  containerDOMEl.innerHTML += 
-  ` 
-  <div class="img-container">
-    <img src="${image.image}">
-    <div class="img-text">
-    <h2>${image.title}</h2>
-    <p>${image.text}</p>
-    </div>
-  </div>
-  ` ;
-} 
-
-
-//Dopo che le ho create nell'html, vado a prendermi dal DOM tutti gli elementi con la classe img-container
+//Dopo che li ho creati nell'html, vado a prendermi dal DOM tutti gli elementi con la classe img-container
 const imgContainerDOMEl = document.querySelectorAll(".img-container");
 
 // Fisso una variabile contatore che parte da 0
-let currentImg = 0;
+let currentImgIndex = 0;
 
 // Rendo quindi visibile solo il primo elemento di default
-imgContainerDOMEl[currentImg].classList.add("active");
+imgContainerDOMEl[currentImgIndex].classList.add("active");
 
 
+
+////////////////////////////////// EVENTI ////////////////////////////////
 
 
 // Vado a prendere dal DOM la freccia destra e gli aggancio un evento
@@ -71,22 +124,9 @@ const rightArrowDOMEl = document.querySelector(".arrow-right");
 
 rightArrowDOMEl.addEventListener("click", function(){
 
-  imgContainerDOMEl[currentImg].classList.remove("active");
+  swipeRight ();
 
-  if(currentImg === images.length - 1) {
-
-    currentImg = 0;
-
-  } else {
-
-    currentImg ++;
-
-  }
-   
-  imgContainerDOMEl[currentImg].classList.add("active");
-  
 })
-
 
 
 // Vado a prendere dal DOM la freccia sinistra e gli aggancio un evento
@@ -94,18 +134,9 @@ const leftArrowDOMEl = document.querySelector('.arrow-left');
 
 leftArrowDOMEl.addEventListener('click', function () {
 
-  imgContainerDOMEl[currentImg].classList.remove("active");
-
-  if(currentImg === 0) {
-
-    currentImg = images.length -1;
-
-  } else {
-
-    currentImg --;
-
-  }
-
-  imgContainerDOMEl[currentImg].classList.add("active");
+  swipeLeft()
 
 });
+
+
+////////////////////////////////// FINE EVENTI /////////////////////////////////
